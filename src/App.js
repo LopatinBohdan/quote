9f45153f1ea320805402bbbbc8f8f68aae3482af
LopatinBohdan <lopatin.bogdan0405@gmail.com>
2023-02-23 22:32:34 +0200

    quote

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+
+function Button(props){
+  const BtnClick=()=>{
+    props.onClickAct(props.newVal);
+  }
+  return(
+    <button onClick={BtnClick}>Next Quote</button>
+  );
+}
+
+function Quote(props){
+  return(
+    <div style={{width:"100px"}}>{props.quoteValue}</div>
+  )
+}
 
 function App() {
+  const arr=["quote1", "quote2", "quote3", "quote4"];
+  let random=Math.floor(Math.random()*5);
+
+  const[quoteValue, setQuoteValue]=useState("-1");
+
+  const stateFunc=(random)=>{
+    setQuoteValue(arr[random]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button newVal={random} onClickAct={stateFunc}></Button>
+      <Quote quoteValue={arr[random]}></Quote>
+    </>
   );
 }
 
